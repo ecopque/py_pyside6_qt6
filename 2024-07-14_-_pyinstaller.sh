d6cb65ae6c466b9ea6d60e2710d60fca365d6512
Edson Copque <ecop@disroot.org>
2024-07-14 15:54:10 -0300

    Update 2024-07-14_-_pyinstaller.sh

--- 2024-07-14_-_pyinstaller.sh
+++ 2024-07-14_-_pyinstaller.sh
@@ -1,1 +1,3 @@
+pip install pyinstaller
+pip freeze > 'requirements.txt'
 pyinstaller –name="VsCalculator" --noconfirm --onefile --add-data='files/:files' --icon='files/ecop.png' --noconsole --clean --log-level=WARN main.py
